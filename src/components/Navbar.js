dda05062ebca1768249421242e236e7f0195da2b
collins-coder <chirchircollins9@gmail.com>
2026-06-24 12:48:55 +0300

    alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,78 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
       <div className="logo">
-        <img src="/logo.png" alt="KUNG'AA INITIATIVE" />
+        <img
+          src="/logo.png"
+          alt="KUNG'AA INITIATIVE"
+        />
         <h2>KUNG'AA INITIATIVE</h2>
       </div>
 
-      <ul className="nav-links">
+      <div
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
 
+      <ul
+        className={
+          menuOpen
+            ? "nav-links active"
+            : "nav-links"
+        }
+      >
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            to="/"
+            onClick={() => setMenuOpen(false)}
+          >
+            Home
+          </Link>
         </li>
 
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            to="/about"
+            onClick={() => setMenuOpen(false)}
+          >
+            About
+          </Link>
         </li>
 
         <li>
-          <Link to="/services">Services</Link>
+          <Link
+            to="/services"
+            onClick={() => setMenuOpen(false)}
+          >
+            Services
+          </Link>
         </li>
 
         <li>
-          <Link to="/leadership">Leadership</Link>
+          <Link
+            to="/leadership"
+            onClick={() => setMenuOpen(false)}
+          >
+            Leadership
+          </Link>
         </li>
 
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link
+            to="/contact"
+            onClick={() => setMenuOpen(false)}
+          >
+            Contact
+          </Link>
         </li>
-
       </ul>
 
     </nav>
